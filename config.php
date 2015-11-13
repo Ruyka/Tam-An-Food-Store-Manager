70b492f801c7221931a58410fb779dab3fba117c
DeKal <hhphat@apcs.vn>
2015-11-13 18:38:49 +0700

    add print Receipt interface fix errors in config file

--- config.php
+++ config.php
@@ -1,47 +1,41 @@
 <?php
  
-/*
-    The important thing to realize is that the config file should be included in every
-    page of your project, or at least any page you want access to these settings.
-    This allows you to confidently use these settings throughout a project because
-    if something changes such as your database credentials, or a path to a specific resource,
-    you'll only need to update it here.
-*/
+
     //store database
     defined("DATABASE_PATH")
-        or define("DATABASE_PATH", realpath(dirname(__FILE__) . '/database/' ));            
+        or define("DATABASE_PATH", realpath(dirname(__FILE__) . '/database/' ).'/');            
     //store include files: to create View of HTML, class and global function
     defined("INCLUDES_PATH")
-        or define("INCLUDES_PATH", realpath(dirname(__FILE__) . '/includes/' ));
+        or define("INCLUDES_PATH", realpath(dirname(__FILE__) . '/includes/' ).'/');
     //store javascript files to support user interaction with our website
     defined("JAVASCRIPT_PATH")
-        or define("JAVASCRIPT_PATH", realpath(dirname(__FILE__) . '/js/' ));        
+        or define("JAVASCRIPT_PATH", realpath(dirname(__FILE__) . '/js/' ).'/');        
     //store library 
     defined("LIBRARY_PATH")
-        or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/lib/' ));
+        or define("LIBRARY_PATH", realpath(dirname(__FILE__) . '/lib/' ).'/');
     //store images, sound or flash files
     defined("MEDIA_PATH")
-        or define("MEDIA_PATH", realpath(dirname(__FILE__) . '/media/' ));
+        or define("MEDIA_PATH", realpath(dirname(__FILE__) . '/media/' ).'/');
     // store the main code of the project (html,php)
     defined("PUBLIC_HTML_PATH")
-        or define("PUBLIC_HTML_PATH", realpath(dirname(__FILE__) . '/public_HTML/' ));
+        or define("PUBLIC_HTML_PATH", realpath(dirname(__FILE__) . '/public_HTML/' ).'/');
     //store css file
     defined("STYLE_PATH")
-        or define("STYLE_PATH", realpath(dirname(__FILE__) . '/style/' ));
+        or define("STYLE_PATH", realpath(dirname(__FILE__) . '/style/' ).'/');
     //store class file (object) used in the project
     defined("CLASS_PATH")
-        or define("CLASS_PATH", realpath(dirname(__FILE__) . '/class/' ));
+        or define("CLASS_PATH", realpath(dirname(__FILE__) . '/class/' ).'/');
     //store global function handle the unrelated to class problem
     defined("FUNCTION_PATH")
-        or define("FUNCTION_PATH", realpath(dirname(__FILE__) . '/includes/function/' ));
+        or define("FUNCTION_PATH", realpath(dirname(__FILE__) . '/includes/function' ).'/');
         
     //the interface of website will be slit into different views and we store it in view
     defined("VIEW_PATH")
-        or define("VIEW_PATH", realpath(dirname(__FILE__) . '/includes/view/' ));
+        or define("VIEW_PATH", realpath(dirname(__FILE__) . '/includes/view'  ).'/');
         
     //store the images, background,...
     defined("IMAGE_PATH")
-        or define("IMAGE_PATH", realpath(dirname(__FILE__) . '/media/image/' ));
+        or define("IMAGE_PATH", realpath(dirname(__FILE__) . '/media/image/' ).'/');
     
     //if IS_TEST is true then we test the project in PROJECT folder with WAMP
     defined("IS_TEST") or define("IS_TEST",true);
---
+++ includes/view/header.php
@@ -0,0 +1,36 @@
+<!-- 
+    Header part of Tam An webpage
+    can import when has include config.php
+ -->
+<div id="topbar">
+    <div class="fixedwidth">
+        <div id="logodiv">
+            <img id="logo" src="<?php echo CONFIG_PATH('image')."logo.jpg"?>"  />
+        </div>
+        <div id="sign-in">
+            <img src="sign-in.PNG" /> Sign In
+        </div>
+
+        <div id="topmenudiv">
+            <ul>
+                <li> <p>Home </p> </li>
+                <li> <p>Service </p></li>
+                <li> <p>Contact Us </p></li>
+            </ul>
+        </div>
+        
+        <div id="searchdiv">
+            <input type="text" placeholder="Search ..."/>
+        </div>
+    </div>
+</div>
+
+<div class="break" > </div>
+
+<div id="newsbar">
+    <div class="fixedwidth">
+        <p id="newsheader"> ORGANICFOOD <span id="VN"> VIỆT NAM </span> </p>
+    </div>
+    
+</div>
+<div class="break"> </div>
