803e3e4df26feeb0368866c09b6e4605ea4b3c19
DeKal <hhphat@apcs.vn>
2015-11-21 10:38:44 +0700

    fix bug

--- includes/class/AllClass.php
+++ includes/class/AllClass.php
@@ -1,13 +1,10 @@
 <?php
 
 	require_once($_SERVER["DOCUMENT_ROOT"] . 'Tam-An-Food-Store-Manager/'. 'config.php');
-	
-	
 	require_once(CLASS_PATH."BasicInfo.php");
 	require_once(CLASS_PATH."Customer.php");
 	require_once(CLASS_PATH."Database.php");
 	require_once(CLASS_PATH."Employee.php");
-	require_once(CLASS_PATH."Management.php");
 	require_once(CLASS_PATH."Product.php");
 	require_once(CLASS_PATH."ProductFactory.php");
 	require_once(CLASS_PATH."Receipt.php");
